8e429b6bb3c334e89c0ae2d1127119d2feac9982
RahomotullahPappu <rahomotullah72778@gmail.com>
2023-05-30 03:06:49 +0600

    added carts collection and user collection apis delete operation and post operation with mongodb server

--- index.js
+++ index.js
@@ -26,48 +26,83 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const usersCollection = client.db("bistroDb").collection("users");
     const menuCollection = client.db("bistroDb").collection("menu");
     const reviewCollection = client.db("bistroDb").collection("reviews");
     const cartCollection = client.db("bistroDb").collection("carts");
-    
-    //menu data
-    app.get('/menu', async(req, res) =>{
+
+
+    // users collection apis
+     app.get('/users', async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+     })
+
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+
+      const query = { email: user.email }
+      const existingUser = await usersCollection.findOne(query);
+
+      if (existingUser) {
+        return res.send({ message: 'user already exists' })
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    })
+
+    app.patch('/users/admin/:id', async(req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const filter = {_id: new ObjectId(id)}
+      const updateDoc = {
+        $set: {
+         role: 'admin'
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    })
+
+    //menu collection apis
+    app.get('/menu', async (req, res) => {
       const result = await menuCollection.find().toArray();
       res.send(result);
-  })
+    })
 
-   //reviews data 
-    app.get('/reviews', async(req, res) =>{
+    //reviews collection apis
+    app.get('/reviews', async (req, res) => {
       const result = await reviewCollection.find().toArray();
       res.send(result);
-  })
+    })
 
 
-  //carts collection apis
-  app.get('/carts', async(req, res) => {
-    const email = req.query.email;
-   
-    if(!email){
-      res.send([]);
-    }
-    const query = { email: email }
-    const result = await cartCollection.find(query).toArray();
-    res.send(result);
-  })
+    //carts collection apis
+    app.get('/carts', async (req, res) => {
+      const email = req.query.email;
 
-  app.post('/carts', async(req, res) => {
-    const item = req.body;
-    console.log(item);
-    const result = await cartCollection.insertOne(item);
-    res.send(result);
-  });
+      if (!email) {
+        res.send([]);
+      }
+      const query = { email: email }
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    })
 
-  app.delete('/carts/:id', async (req, res) => {
-    const id = req.params.id;
-    const query = { _id: new ObjectId(id) };
-    const result = await cartCollection.deleteOne(query);
-    res.send(result);
-  })
+    app.post('/carts', async (req, res) => {
+      const item = req.body;
+      const result = await cartCollection.insertOne(item);
+      res.send(result);
+    });
+
+    app.delete('/carts/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+    })
+
+  
 
 
     // Send a ping to confirm a successful connection
